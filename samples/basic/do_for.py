ceeb9d4584a2ed8c0a3292c10ce8c453e7c38548
severnchen <severnchen@tencent.com>
2021-04-30 15:17:18 +0800

    add:for 遍历方式

--- samples/basic/do_for.py
+++ samples/basic/do_for.py
@@ -9,3 +9,30 @@ for name in names:
 # 打印数字 0 - 9
 for x in range(10):
     print(x)
+
+# Python 遍历List三种方式 https://www.cnblogs.com/pizitai/archive/2017/02/14/6398276.html
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+if __name__ == '__main__':
+    list = ['html', 'js', 'css', 'python']
+
+    # 方法1
+    print '遍历列表方法1：'
+    for i in list:
+        print ("序号：%s   值：%s" % (list.index(i) + 1, i))
+
+    print '\n遍历列表方法2：'
+    # 方法2
+    for i in range(len(list)):
+        print ("序号：%s   值：%s" % (i + 1, list[i]))
+
+    # 方法3
+    print '\n遍历列表方法3：'
+    for i, val in enumerate(list):
+        print ("序号：%s   值：%s" % (i + 1, val))
+
+    # 方法3
+    print '\n遍历列表方法3 （设置遍历开始初始位置，只改变了起始序号）：'
+    for i, val in enumerate(list, 2):
+        print ("序号：%s   值：%s" % (i + 1, val))
